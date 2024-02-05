661a4798a529a0a1f02299da450bc3ee51d7c2eb
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-05 21:11:20 +0530

    Add paymentsCategories migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_05_152749) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_05_153115) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_05_152749) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_categories_on_author_id"
+  end
+
+  create_table "category_payments", force: :cascade do |t|
+    t.bigint "payment_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_category_payments_on_category_id"
+    t.index ["payment_id"], name: "index_category_payments_on_payment_id"
   end
 
   create_table "payments", force: :cascade do |t|
@@ -39,5 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_05_152749) do
   end
 
   add_foreign_key "categories", "users", column: "author_id"
+  add_foreign_key "category_payments", "categories"
+  add_foreign_key "category_payments", "payments"
   add_foreign_key "payments", "users", column: "author_id"
 end
