da114396aa168842c2bc83f20f7b8dcda3111382
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-06 12:23:21 +0530

    Fix rubocop errors

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,14 +1,14 @@
 module CategoriesHelper
-    def total_amount(category)
-        category.payments.sum(:amount)
-      end
-    
-      def check_url(url)
-        default = 'no-pictures.png'
-        if (File.extname(url) =~ /^(.png|.gif|.jpg)$/) || (url =~ /^#{URI::DEFAULT_PARSER.make_regexp}$/)
-          url
-        else
-          default
-        end
-      end
+  def total_amount(category)
+    category.payments.sum(:amount)
+  end
+
+  def check_url(url)
+    default = 'no-pictures.png'
+    if (File.extname(url) =~ /^(.png|.gif|.jpg)$/) || (url =~ /^#{URI::DEFAULT_PARSER.make_regexp}$/)
+      url
+    else
+      default
+    end
+  end
 end
