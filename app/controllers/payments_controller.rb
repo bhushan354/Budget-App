9bda249ea9ab73c9bef7c6b6c7c92996c87d3ace
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-06 22:14:43 +0530

    fix rubcop errors

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,40 +1,39 @@
 class PaymentsController < ApplicationController
-    before_action :set_category, only: [:index, :new, :create]
-  
-    def index
-      @payments = @category.payments.order(id: :desc)
+  before_action :set_category, only: %i[index new create]
+
+  def index
+    @payments = @category.payments.order(id: :desc)
+  end
+
+  def new
+    @payment = Payment.new
+  end
+
+  def create
+    @payment = current_user.payments.build(payment_params)
+    @categories = Category.where(id: payment_params[:category_ids])
+
+    if @payment.save
+      @categories.each { |category| category.payments << @payment unless category.payments.include?(@payment) }
+      flash[:success] = 'Transaction was created!'
+      redirect_to category_payments_path(@category)
+    else
+      flash.now[:error] = @payment.errors.full_messages.to_sentence
+      render :new
     end
-  
-    def new
-      @payment = Payment.new
-    end
-  
-    def create
-      @payment = current_user.payments.build(payment_params)
-      @categories = Category.where(id: payment_params[:category_ids])
-  
-      if @payment.save
-        @categories.each { |category| category.payments << @payment unless category.payments.include?(@payment) }
-        flash[:success] = 'Transaction was created!'
-        redirect_to category_payments_path(@category)
-      else
-        flash.now[:error] = @payment.errors.full_messages.to_sentence
-        render :new
-      end
-    end
-  
-    private
-  
-    def set_category
-      @category = Category.find_by(id: params[:category_id])
-      unless @category
-        flash[:error] = 'Category not found'
-        redirect_to categories_path
-      end
-    end
-  
-    def payment_params
-      params.require(:payment).permit(:name, :amount, category_ids: [])
-    end
+  end
+
+  private
+
+  def set_category
+    @category = Category.find_by(id: params[:category_id])
+    return if @category
+
+    flash[:error] = 'Category not found'
+    redirect_to categories_path
+  end
+
+  def payment_params
+    params.require(:payment).permit(:name, :amount, category_ids: [])
+  end
 end
-  
