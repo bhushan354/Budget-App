024b889fe1724bf957ece8b3ce5e983bd8b06081
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-08 07:34:01 +0530

    Fix before action error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+    # This will ensure that the authenticate_user! method is defined before it is used as a callback.
+    before_action :authenticate_user!
 end
