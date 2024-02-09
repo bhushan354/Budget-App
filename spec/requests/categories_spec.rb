cef75d5af491d9f2622952f01030021d6b712653
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-09 11:42:47 +0530

    Add tests for categories

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,7 +1,57 @@
 require 'rails_helper'
 
-RSpec.describe 'Categories', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'categories', type: :request do
+  let(:valid_attributes) { { 'user' => @user, 'name' => 'Food', 'icon' => 'missing_avatar.png' } }
+
+  before :each do
+    @user = User.create(name: 'Tom', email: 'tom@example.com', password: 'topsecret')
+    login(@user)
+  end
+
+  context 'POST /categories' do
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  context 'GET /categories' do
+    before :each do
+      get categories_path
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'renders the correct content' do
+      expect(response.body).to include('ADD CATEGORY')
+    end
+  end
+
+  context 'GET /categories/new' do
+    before :each do
+      get new_category_path
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the new template' do
+      expect(response).to render_template(:new)
+    end
+  end
+
+  def login(user)
+    post new_user_session_path, params: {
+      user: {
+        email: user.email, password: user.password
+      }
+    }
+    follow_redirect!
   end
 end
