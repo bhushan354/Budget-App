91bbe4d181d1f0784e8e2d5adcd37b6e66138049
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-09 11:45:24 +0530

    add tests for pages

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,7 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Pages', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  before :each do
+    @user = User.create(name: 'Tom', email: 'tom@example.com', password: 'topsecret')
+    login(@user)
+  end
+
+  context 'GET /splash' do
+    before :each do
+      get '/pages/splash'
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the right view file' do
+      expect(response).to render_template(:splash)
+    end
+
+    it 'renders the right placeholder' do
+      expect(response.body).to include('<h1>Budget Tracker</h1>')
+    end
+  end
+
+  def login(user)
+    post new_user_session_path, params: {
+      user: {
+        email: user.email, password: user.password
+      }
+    }
+    follow_redirect!
   end
 end
