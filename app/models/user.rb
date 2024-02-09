8d2e0f5254ae926f7f9711455a1da6d4b371af6a
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-09 16:07:28 +0530

    Rubocop errors fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   has_many :categories, dependent: :destroy, foreign_key: :author_id
   has_many :payments, dependent: :destroy, foreign_key: :author_id
 
- validates :email, presence: true
+  validates :email, presence: true
 end
